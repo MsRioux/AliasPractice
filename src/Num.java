37072b101da39ad733306f1a1e810a6a4d84bed0
MsRioux <jrioux@mvusd.net>
2019-10-14 15:48:26 -0700

    Initial commit

---
+++ src/Num.java
@@ -0,0 +1,20 @@
+public class Num
+{
+    private int value;
+
+    public Num (int value)
+    {
+        this.value = value;
+    }//end constructor
+
+    public void setValue(int value)
+    {
+        this.value = value;
+    }//end setValue
+
+    public String toString()
+    {
+        return "Value is " + value;
+    }//end toString
+
+}//end class Num
